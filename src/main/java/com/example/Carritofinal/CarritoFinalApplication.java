9b801d0abc94d6149983227bd7d7b8170f1f2803
SILVANA <silespindola87@gmail.com>
2023-06-11 15:35:43 -0300

    Initial commit

---
+++ src/main/java/com/example/Carritofinal/CarritoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.Carritofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarritoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarritoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Carritofinal/CarritoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Carritofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarritoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
